025d1e7e86e38afef10d2e9a946374695ef297d4
nati schmidt <nati.schmidt97@outlook.de>
2023-05-10 18:35:41 +0200

    array of card to display in grid, images not showing up WIP

--- App.js
+++ App.js
@@ -3,31 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 import images from './images';
 import {useState} from "react";
 
+
 export default function App() {
 
-    const [cards, setCards] = useState(...images,...images);
+    const [cards, setCards] = useState([...images,...images]);
 
     return (
         <View style={styles.container}>
             <Text> memory game </Text>
-            <div>
-                <div className="board">
+            <View>
+                <View style={styles.board}>
                     {cards.map((card, index) => (
-                        <div className="cards-outline" key={index}>
-                            <div className="cards">
-                                <div className="card-front">
-                                    <img src={card} alt="" />
-                                </div>
-                                <div className="card-back"></div>
-                            </div>
-                        </div>
+                        <View style={styles.cardOutline} key={index}>
+                            <View style={styles.card}>
+                                <View style={styles.cardFront}>
+                                    <Image source={card} style={styles.cardImage} />
+                                </View>
+                                <View style={styles.cardBack}></View>
+                            </View>
+                        </View>
                     ))}
-                </div>
-            </div>
+                </View>
+            </View>
             <StatusBar style="auto" />
         </View>
     );
-
 }
 const styles = StyleSheet.create({
   container: {
@@ -36,4 +36,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#fff',
+    },
+    board: {
+        width: 'calc(100vh - 240px)',
+        maxWidth: 'calc(100vw - 40px)',
+        display: 'grid',
+        gridGap: 10,
+        margin: 'auto',
+        gridTemplateColumns: 'repeat(6, 1fr)',
+    },
+    cardOutline: {
+        perspective: 1000,
+        height: 'calc((100vh - 290px) / 6)',
+        maxHeight: 'calc((100vw - 90px) / 6)',
+    },
+    card: {
+        borderRadius: 5,
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        transform: [{ rotateY: '180deg' }],
+        transformStyle: 'preserve-3d',
+        transitionDuration: '0.3s',
+    },
+    cardFront: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+    },
+    cardImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'contain',
+    },
+    cardBack: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'blue',
+        borderRadius: 10,
+    },
 });
