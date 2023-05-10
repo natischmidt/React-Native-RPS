1305b2c193a192e24defb802e811e4fa5e38a510
nati schmidt <nati.schmidt97@outlook.de>
2023-05-10 18:25:02 +0200

    array of card to display in grid, images not showing up WIP

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import images from './images';
+import {useState} from "react";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text> testtttttttttt </Text>
-      <div>
-      <div className="board">
-          .
-      </div>
 
+    const [cards, setCards] = useState(...images,...images);
 
-      </div>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text> memory game </Text>
+            <div>
+                <div className="board">
+                    {cards.map((card, index) => (
+                        <div className="cards-outline" key={index}>
+                            <div className="cards">
+                                <div className="card-front">
+                                    <img src={card} alt="" />
+                                </div>
+                                <div className="card-back"></div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <StatusBar style="auto" />
+        </View>
+    );
+
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
