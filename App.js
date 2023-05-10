3e9969ff546900802126ded260cf03fa70e90793
nati schmidt <nati.schmidt97@outlook.de>
2023-05-10 18:11:53 +0200

    added some images for the game

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import images from './images';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> testtttttttttt </Text>
+      <div>
+      <div className="board">
+          .
+      </div>
+
+
+      </div>
       <StatusBar style="auto" />
     </View>
   );
