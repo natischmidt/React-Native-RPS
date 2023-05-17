69cfdf171d07b59c83d97d691482c43f597da5c3
nati schmidt <nati.schmidt97@outlook.de>
2023-05-17 21:19:10 +0200

    added naviagtion

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Cards from "./Cards";
 import Login from "./Login";
 
-
-
+const Stacked = createNativeStackNavigator();
 
 export default function App() {
 
-    return <Login />;
+    return (
+        <NavigationContainer>
+            <Stacked.Navigator>
+                <Stacked.Screen name='Login' component={Login}/>
+                <Stacked.Screen name='Cards' component={Cards}/>
+            </Stacked.Navigator>
+        </NavigationContainer>
 
-    // <Cards />;
+    );
 }
--- Login.js
+++ Login.js
@@ -8,6 +8,7 @@ export default function Login() {
 
     const login = () => {
 
+
     };
 
     return (
