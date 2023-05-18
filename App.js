58231a197bdba4b16da60d7460d1adc4d9c0af29
nati schmidt <nati.schmidt97@outlook.de>
2023-05-18 17:25:16 +0200

    added screens WIP

--- App.js
+++ App.js
@@ -1,36 +1,25 @@
-// import React from 'react';
-// // import { NavigationContainer } from '@react-navigation/native';
-// // import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// // import Cards from "./memory wip/Cards";
-// // import Login from "./memory wip/Login";
-//
-// // const Stacked = createNativeStackNavigator();
-//
-// export default function App() {
-//
-//     return (
-//         <NavigationContainer>
-//             <Stacked.Navigator>
-//                 <Stacked.Screen name='Login' component={Login}/>
-//                 <Stacked.Screen name='Cards' component={Cards}/>
-//             </Stacked.Navigator>
-//         </NavigationContainer>
-//
-//     );
-// }
-
 import React from 'react';
-import { View } from 'react-native';
-import LoginPage from './screens/LoginPage';
-import GamePage from './screens/GamePage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from "./screens/LoginPage";
+import GamePage from "./screens/GamePage";
+import {HomePage} from "./screens/HomePage";
 
-const App = () => {
+
+
+
+const Stacked = createNativeStackNavigator();
+
+export default function App () {
     return (
-        <View>
-            {/*<LoginPage />*/}
-            <GamePage />
-        </View>
+        <NavigationContainer>
+           <Stacked.Navigator>
+               <Stacked.Screen name='Home' component={HomePage}/>
+           <Stacked.Screen name='Login' component={LoginPage}/>
+          <Stacked.Screen name='Cards' component={GamePage}/>
+          </Stacked.Navigator>
+        </NavigationContainer>
     );
 };
 
-export default App;
+
--- screens/GamePage.js
+++ screens/GamePage.js
@@ -1,69 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import {Rps} from "../services/API";
+
 
 const GamePage = () => {
-    const [gameInfo, setGameInfo] = useState(null);
-
-    useEffect(() => {
-        const startInterval = setInterval(() => {
-            Rps.getGameInfoFromGame(Rps.getGameId())
-                .then(game => {
-                    setGameInfo(game);
-                    if (game.playerOne !== null && game.playerTwo !== null) {
-                        clearInterval(startInterval);
-                        // waitingForMove();
-                    }
-                })
-                .catch(error => console.log(error));
-        }, 500);
-    }, []);
-
-    // const waitingForMove = async () => {
-    //     clearInterval(startInterval);
-    //     await refreshFn();
-    //     const timer = setInterval(() => {
-    //         getGameInfo()
-    //             .then(game => {
-    //                 if (game?.playerMove !== null && game?.opponentMove !== null) {
-    //                     refreshFn();
-    //                     checkResult(
-    //                         game?.player1.username,
-    //                         game?.player2.username,
-    //                         game?.playerMove,
-    //                         game?.opponentMove
-    //                     );
-    //                     clearInterval(timer);
-    //                 }
-    //             })
-    //             .catch(error => console.log(error));
-    //     }, 1000);
+    // const [gameInfo, setGameInfo] = useState(null);
+    // //
+    // // useEffect(() => {
+    // //     const startInterval = setInterval(() => {
+    // //         Rps.getGameInfoFromGame(Rps.getGameId())
+    // //             .then(game => {
+    // //                 setGameInfo(game);
+    // //                 if (game.playerOne !== null && game.playerTwo !== null) {
+    // //                     clearInterval(startInterval);
+    // //                     // waitingForMove();
+    // //                 }
+    // //             })
+    // //             .catch(error => console.log(error));
+    // //     }, 500);
+    // // }, []);
+    //
+    // // const waitingForMove = async () => {
+    // //     clearInterval(startInterval);
+    // //     await refreshFn();
+    // //     const timer = setInterval(() => {
+    // //         getGameInfo()
+    // //             .then(game => {
+    // //                 if (game?.playerMove !== null && game?.opponentMove !== null) {
+    // //                     refreshFn();
+    // //                     checkResult(
+    // //                         game?.player1.username,
+    // //                         game?.player2.username,
+    // //                         game?.playerMove,
+    // //                         game?.opponentMove
+    // //                     );
+    // //                     clearInterval(timer);
+    // //                 }
+    // //             })
+    // //             .catch(error => console.log(error));
+    // //     }, 1000);
+    // // };
+    //
+    // const refreshFn = async () => {
+    //     const response = await getGameInfo();
+    //     console.log('refreshed data', response);
+    //     const { opponentMove: player2Move, playerMove: player1Move, gameStatus } = response;
+    //
+    //     console.log(player2Move, player1Move);
+    //     if (player1Move) {
+    //         console.log('Update player1 move:', player1Move);
+    //     }
+    //
+    //     if (player2Move) {
+    //
+    //         console.log('Update player2 move:', player2Move);
+    //     }
+    //
+    //
     // };
-
-    const refreshFn = async () => {
-        const response = await getGameInfo();
-        console.log('refreshed data', response);
-        const { opponentMove: player2Move, playerMove: player1Move, gameStatus } = response;
-
-        console.log(player2Move, player1Move);
-        if (player1Move) {
-            console.log('Update player1 move:', player1Move);
-        }
-
-        if (player2Move) {
-
-            console.log('Update player2 move:', player2Move);
-        }
-
-
-    };
-
-    const checkResult = (playerOne, playerTwo, playerMove, opponentMove) => {
-
-    };
-    const choose = (choice) => {
-
-    };
+    //
+    // const checkResult = (playerOne, playerTwo, playerMove, opponentMove) => {
+    //
+    // };
+    // const choose = (choice) => {
+    //
+    // };
 
     return (
         <View style={styles.container}>
