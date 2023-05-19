86c858e242f41dd29840716f9b6d704cd430b165
nati schmidt <nati.schmidt97@outlook.de>
2023-05-19 17:41:18 +0200

    gamepage and game WIP

--- screens/Game.js
+++ screens/Game.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import IP_URL from "../services/IP";
 import {getData} from "./HomePage";
+import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 
 
 
@@ -24,4 +25,67 @@ const MakeMove = async (token, sign) => {
     }
 }
 
-const Game
+const Game = () => {
+
+    const [player, setPlayer] = useState('');
+    const [playerMove, setPlayerMove] = useState('');
+    const [opponent, setOpponent] = useState('');
+    const [opponentMove, setOpponentMove] = useState('');
+    const [result, setResult] = useState('');
+
+
+
+    const HandleMove = async (token, move) => {
+        await MakeMove(token, move)
+            .then(async () => console.log());
+    };
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.choicesContainer}>
+
+                <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('rock')} id="rock">
+                    <Image source={require('../images/rock.png.bmp')} style={[styles.image, styles.choiceImage]} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('scissors')} id="scissor">
+                    <Image source={require('../images/scissor.png.bmp')} style={[styles.image, styles.choiceImage]} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('paper')} id="paper">
+                    <Image source={require('../images/paper.png.bmp')} style={[styles.image, styles.choiceImage]} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+
+
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    choicesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+    },
+    choiceButton: {
+        width: 130,
+        height: 80,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    image: {},
+    choiceImage: {
+        marginTop: 400,
+        width: 150,
+        height: 180,
+        resizeMode: 'contain',
+    },
+});
+
--- screens/GamePage.js
+++ screens/GamePage.js
@@ -65,7 +65,7 @@ const GamePage = () => {
 
     const handleStartGame = async () => {
         await StartGame()
-            .then( (response) => {
+            .then((response) => {
                 storeData('gameId', response.gameStatusId);
                 //wip
             })
@@ -74,55 +74,23 @@ const GamePage = () => {
     const handleJoin = async (gameId) => {
         await JoinGame(gameId)
             .then(() => {
-                storeData('gameId',gameId);
+                storeData('gameId', gameId);
                 //wip
             });
     };
 
+
     return (
-        <View style={styles.container}>
-                <View style={styles.choicesContainer}>
 
-                    <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('rock')} id="rock">
-                        <Image source={require('../images/rock.png.bmp')} style={[styles.image, styles.choiceImage]} />
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('scissors')} id="scissor">
-                        <Image source={require('../images/scissor.png.bmp')} style={[styles.image, styles.choiceImage]} />
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={[styles.button, styles.choiceButton]} onPress={() => choose('paper')} id="paper">
-                        <Image source={require('../images/paper.png.bmp')} style={[styles.image, styles.choiceImage]} />
-                    </TouchableOpacity>
-                </View>
+        <View>
+            wip
         </View>
-    );
+
+    )
+
+
 };
 
-const styles = StyleSheet.create({
-    container: {
-    },
-    choicesContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 20,
-    },
-    choiceButton: {
-        width: 130,
-        height: 80,
-        borderRadius: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    image: {},
-    choiceImage: {
-        marginTop: 400,
-        width: 150,
-        height: 180,
-        resizeMode: 'contain',
-    },
-});
 
 export default GamePage;
 
