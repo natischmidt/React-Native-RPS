d1895a4a2d0536e8d175b72f55575980d07e1f97
nati schmidt <nati.schmidt97@outlook.de>
2023-06-03 13:15:29 +0200

    result functions trouble shooting with some console.log both players can make their moves, they are mapped correclty but the result alert function isnt triggered WIP

--- screens/Game.js
+++ screens/Game.js
@@ -14,127 +14,6 @@ const Game = () => {
     const [result, setResult] = useState("");
     const [gameStatus, setGameStatus] = useState(null);
 
-    useEffect(() => {
-        const interval = setInterval(GameStatus, 2000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
-    const GameStatus = async () => {
-        try {
-            const token = await AsyncStorage.getItem('token');
-
-            const response = await axios.get(
-                IP_URL + '/games/status',
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        token,
-                    }
-                }
-            );
-
-            setGameStatus(response.data);
-            console.log(response.data);
-
-            // if (response.data.playerMove && response.data.opponentMove) {
-            //     setPlayerMove(response.data.playerMove);
-            //     setOpponentMove(response.data.opponentMove);
-            //     setResult(response.data.result);
-            //     handleResult(response.data.result, token);
-            // }
-
-            if (response.data.playerMove !== null && response.data.opponentMove !== null) {
-                setPlayerMove(response.data.playerMove);
-                setOpponentMove(response.data.opponentMove);
-                setResult(response.data.result);
-                handleResult(response.data.result, token);
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    const handleMove = async (sign) => {
-        try {
-            const token = await AsyncStorage.getItem('token');
-
-            const response = await axios.post(
-                IP_URL + '/games/move/' + sign,
-                null,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        token,
-                    }
-                }
-            );
-
-            if (response.status !== 200) {
-                console.error(response);
-                throw new Error('Failed to make a move');
-            }
-
-            const moveResponse = response.data;
-
-            setPlayerMove(moveResponse.playerMove);
-            setOpponentMove(moveResponse.opponentMove);
-            setResult(moveResponse.result);
-
-
-            if (moveResponse.opponentMove !== null) {
-                handleResult(moveResponse.result, token);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-
-    // const handleMove = async (sign) => {
-    //     try {
-    //         const gameid = await getData("gameid");
-    //         const token = await getData("token");
-    //
-    //         const gameContainer = {
-    //             uuid: gameid,
-    //             playerMove: sign,
-    //             opponentMove: opponentMove,
-    //         };
-    //
-    //         console.log(gameContainer.playerMove);
-    //
-    //         const response = await axios.post(IP_URL + '/games/move/' + sign, {
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'token': token,
-    //             },
-    //
-    //         });
-    //
-    //         if (response.status !== 200) {
-    //             console.error(response);
-    //             throw new Error('Failed to make a move');
-    //         }
-    //
-    //         const moveResponse = response.data;
-    //
-    //         console.log(moveResponse);
-    //         console.log(gameContainer)
-    //         console.log(sign);
-    //         console.log(playerMove, opponentMove)
-    //
-    //         setPlayerMove(moveResponse.playerMove);
-    //         setOpponentMove(moveResponse.opponentMove);
-    //         setResult(moveResponse.result);
-    //
-    //         handleResult(moveResponse.result, token);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-
     const handleResult = (result, token) => {
         if (result === "WIN" && token) {
             Alert.alert(":)", "You won!", [
@@ -165,6 +44,84 @@ const Game = () => {
             ]);
         }
     };
+
+    useEffect(() => {
+        const interval = setInterval(GameStatus, 2000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    const GameStatus = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+
+            const response = await axios.get(
+                IP_URL + '/games/status',
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        token,
+                    }
+                }
+            );
+
+            setGameStatus(response.data);
+            console.log(response.data);
+            console.log(response.data.playerMove);
+            console.log(response.data.opponentMove);
+            console.log(response.data.result);
+
+            if (response.data.playerMove !== null && response.data.opponentMove !== null) {
+                setPlayerMove(response.data.playerMove);
+                setOpponentMove(response.data.opponentMove);
+                setResult(response.data.result);
+                handleResult(response.data.result, token);
+            }
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const handleMove = async (sign) => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+
+            const response = await axios.post(
+                IP_URL + '/games/move/' + sign,
+                null,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        token,
+                    }
+                }
+            );
+
+            if (response.status !== 200) {
+                console.error(response);
+                throw new Error('Failed to make a move');
+            }
+
+            const moveResponse = response.data;
+
+            setPlayerMove(moveResponse.playerMove);
+            setOpponentMove(moveResponse.opponentMove);
+            setResult(moveResponse.result);
+            console.log(response.data.result);
+            console.log(moveResponse.result);
+
+
+            if (moveResponse.opponentMove !== null) {
+                handleResult(moveResponse.result, token);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
 
     return (
         <View style={styles.container}>
